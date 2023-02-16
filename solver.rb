915b5ba8479b9c41fd45dfe88c1a104b2e57c8fd
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-16 23:18:05 +0200

    Fix linter error

--- solver.rb
+++ solver.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # TDD project - the following code is solved after Test is written first
 class Solver
   def factorial(number: 0)
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './solver'
 
 RSpec.describe Solver do
