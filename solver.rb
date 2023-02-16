c20df3fbd044561f0e56f4f2ae97570876184291
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-16 19:26:17 +0200

    Update the code for factorial method

--- solver.rb
+++ solver.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 class Solver
-  
-  def factorial(n:0)
-    if n<0
-      raise "Number cannot be less than zero."
-    else
-      n
+  def factorial(number: 0)
+    raise 'Number cannot be less than zero.' if n.negative?
+
+    counter = 1
+    fact = 1
+    while counter <= number
+      fact *= counter
+      counter += 1
     end
+    fact
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 require './solver'
 
-RSpec.describe Solver do 
-  describe '#factorial' do 
+RSpec.describe Solver do
+  describe '#factorial' do
     it 'returns an integer' do
       expect(subject.factorial).to be_integer
     end
     context 'when N is a number less than zero' do
       it 'should raise an exception' do
-        expect  {subject.factorial n: -1}.to raise_error('Number cannot be less than zero.')
-    end
-    
-    context 'When N is a positive integer' do 
-     it 'returns 1 when N is 0' do
-        actual = subject.factorial( n: 0)
-        res = 1
-        expect(actual).to eql(res)
+        expect { subject.factorial number: -1 }.to raise_error('Number cannot be less than zero.')
       end
 
-      it 'return 24 when N is 4' do 
-        actual = subject.factorial( n: 4)
-        res = 24
-        expect(actual).to eq(res)
+      context 'When N is a positive integer' do
+        it 'returns 1 when N is 0' do
+          actual = subject.factorial(number: 0)
+          res = 1
+          expect(actual).to eql(res)
+        end
+
+        it 'return 24 when N is 4' do
+          actual = subject.factorial(number: 4)
+          res = 24
+          expect(actual).to eq(res)
+        end
       end
     end
   end
-end
 end
