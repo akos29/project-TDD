6ba6fd4f2b4eddc21511c3a51ccd1348b528b099
187jjay187 <187jjay187@gmail.com>
2023-02-17 07:33:15 +0200

    Add defined reverse method

--- solver.rb
+++ solver.rb
@@ -1,4 +1,3 @@
-# TDD project - the following code is solved after Test is written first
 class Solver
   def factorial(number: 0)
     raise 'Number cannot be less than zero.' if number.negative?
@@ -10,5 +9,25 @@ class Solver
       counter += 1
     end
     fact
+  end
+
+  def reverse(word)
+    raise ArgumentError, 'No word was passed' if word.nil?
+
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    raise ArgumentError, 'No argument was passed' if n.nil?
+
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
+    end
   end
 end
